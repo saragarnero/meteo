c075049113f5a6dd90bf3c897973d8e71a624742
Sara Garnero <volpina1986@gmail.com>
2025-11-12 16:24:31 +0100

    aggiornato esercizio

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -8,10 +9,27 @@ import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import { LineChart } from '@mui/x-charts/LineChart';
+import { LineChart } from "@mui/x-charts/LineChart";
 
 function App() {
   const [luogo, setLuogo] = useState("");
+  const [orari, setOrari] = useState([]);
+  const [temperatura, setTemperatura] = useState([]);
+
+  useEffect(() => {
+    async function cercaMeteo(lat, long) {
+      const response = await fetch(
+        "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&forecast_days=1"
+      );
+      const result = await response.json();
+      console.log(result.hourly);
+      setOrari(
+        result.hourly.time?.map((item) => Number(item.slice(11).slice(0, 2)))
+      );
+      setTemperatura(result.hourly.temperature_2m);
+    }
+    cercaMeteo(52.52, 13.41);
+  }, []);
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: "#2d5cdfff",
     ...theme.typography.body2,
@@ -20,11 +38,12 @@ function App() {
     color: (theme.vars ?? theme).palette.text.secondary,
     ...theme.applyStyles("dark", {
       backgroundColor: "#1A2027",
-    }),
+    }), 
     
   }));
   return (
     <Grid container spacing={2}>
+      {JSON.stringify(orari)}
       <Grid size={8}>
         <TextField
           id="outlined-basic"
@@ -42,15 +61,16 @@ function App() {
         <Item>size=4</Item>
       </Grid>
       <Grid size={8}>
+        <Item>size=8</Item>
         <LineChart
-      xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-      series={[
-        {
-          data: [2, 5.5, 2, 8.5, 1.5, 5],
-        },
-      ]}
-      height={600}
-    />npm
+          xAxis={[{ data: orari }]}
+          series={[
+            {
+              data: temperatura,
+            },
+          ]}
+          height={600}
+        />
       </Grid>
     </Grid>
   );
